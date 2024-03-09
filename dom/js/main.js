56061490a223c5467ce602c218cfe3905c8ff40b
AshleyPCalmell <91920494+AshleyPCalmell@users.noreply.github.com>
2024-03-08 17:46:36 -0800

    posting so i cna ge a link

---
+++ dom/js/main.js
@@ -0,0 +1,25 @@
+console.log("hello hell");
+
+let pageTitle = document.querySelector("#page-title");
+
+// javascript timeout changes h1 title after 3 seconds
+setTimeout( function(){
+    pageTitle.style.color = "red";  
+}, 3000);
+
+// click event on header changes background color
+document.querySelector("header").onclick=function(){
+   console.log("clicked header");
+   document.querySelector("body").style.backgroundColor = "black";
+}
+
+// document.querySelector("#image-0").addEventListener("click",function(){
+//     document.querySelector("#image-0").style.visibility="visible";
+//     alert("bark bark");
+// })
+// document.querySelector("#image-1").addEventListener("click",function(){
+//     document.querySelector("#image-1").style.visibility="visible";
+// })
+// document.querySelector("#image-2").addEventListener("click",function(){
+//     document.querySelector("#image-2").style.visibility="visible";
+// })
